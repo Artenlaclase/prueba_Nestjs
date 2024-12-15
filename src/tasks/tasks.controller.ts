5255df6cfaa3a8be4c7bbf78945306310acd4aa2
Artenlaclase <artenlaclase@gmail.com>
2024-12-15 16:36:27 -0300

    get y post

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,9 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+
+
 
 @Controller('tasks')
 export class TasksController {
+
+    constructor(private tasksService: TasksService) {
+
+    }
+
     @Get()
     helloworld() {
-        return 'Hello World'
+        return this.tasksService.getAllTasks();
     }
+    @Post()
+    crateTask(@Body() newTasks: any) {
+        console.log(newTasks);
+        return 'guardando'
+       // this.tasksService.createTasks();
+    }
+
 }
