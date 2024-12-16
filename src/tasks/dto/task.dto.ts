4c50a759ca3b1205a4f02d524c2c233d401cf47f
Artenlaclase <artenlaclase@gmail.com>
2024-12-16 12:56:46 -0300

    Validation

--- src/tasks/dto/task.dto.ts
+++ src/tasks/dto/task.dto.ts
@@ -1,14 +1,30 @@
 import { TaskStatus } from "../task.entity";
+import { IsIn, IsNotEmpty, IsOptional, IsString, MinLength } from 'class-validator';
 
 export class CreateTaskDto {
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(3)
     title: string;
+
+    @IsString()
     description: string;
-    
+
 }
 
 export class UpdateTaskDto {
+    @IsString()
+    @IsOptional()
     title?: string;
+
+    @IsString()
+    @IsOptional()
     description?: string;
-    status?: TaskStatus; 
-    
+
+    @IsString()
+    @IsOptional()
+    @IsIn([TaskStatus.PENDING, TaskStatus.IN_PROGRESS, TaskStatus.DONE])
+    status?: TaskStatus;
+
 }
